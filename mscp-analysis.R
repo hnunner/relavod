709a1fb20a00d94c62b8451202515f949a5c187c
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2017-02-13 14:38:52 +0100

    implemented extraction of LNI sequence

--- mscp-analysis.R
+++ mscp-analysis.R
@@ -37,14 +37,26 @@ importVodSimData <- function(modelType = "default", timestamp = "latest") {
   return(vodSimData)
 }
 
+
+extractLNISequence <- function(vodData) {
+  moves <- vodData[vodData$round >= 1,2:4]
+  moves$lniSequence <- -1
+  moves[moves$player1 == "c" & moves$player2 == "d" & moves$player3 == "d", ]$lniSequence <- 1
+  moves[moves$player1 == "d" & moves$player2 == "c" & moves$player3 == "d", ]$lniSequence <- 2
+  moves[moves$player1 == "d" & moves$player2 == "d" & moves$player3 == "c", ]$lniSequence <- 3
+  return(moves$lniSequence)
+}
+
+
 #----------------------------------------------------------------------------------------------------#
-# function: computeLNI
+# function: computeLNIs
 #   Computation of the Latent Norm Index (LNI) for the given VOD data.
 #   param:  vodData
 #       the VOD data 
 #----------------------------------------------------------------------------------------------------#
-computeLNI <- function(vodData) {
-  vodData
+computeLNIs <- function(vodData) {
+  lniSequence <- extractLNISequence(vodData)
+  print(lniSequence)
 }
 
 #----------------------------------------------------------------------------------------------------#
@@ -55,6 +67,54 @@ analyzeData <- function() {
   vodSimData <- importVodSimData(modelType = "reinf")
   vodSimData <- importVodSimData(modelType = "reinf", timestamp = "20170208-115642")
   vodSimData <- importVodSimData()
-  computeLNI(vodSimData)
+  computeLNIs(vodSimData)
 }
+
+
+############################################# TEST CASES #############################################
+#----------------------------------------------------------------------------------------------------#
+# function: createVodTestData1
+#     Creates the first LNI example data, as in Diekmann & Przepiorka (2016), p.1318
+#----------------------------------------------------------------------------------------------------#
+createVodTestData1 <- function() {
+  round <- c(0,1,2,3,4,5,6,7,8,9,10)
+  player1 <- c(NA,"d","c","c","d","d","c","d","c","d","d")
+  player2 <- c(NA,"c","d","d","d","c","d","d","d","c","d")
+  player3 <- c(NA,"d","d","d","c","d","d","d","d","d","c")
+  util1 <- c(0,100,60,60,100,100,60,0,60,100,100)
+  util2 <- c(0,60,100,100,100,60,100,0,100,60,100)
+  util3 <- c(0,100,100,100,60,100,100,0,100,100,60)
+  return(data.frame(round,player1,player2,player3,util1,util2,util3))
+}
+
+#----------------------------------------------------------------------------------------------------#
+# function: createVodTestData2
+#     Creates the second LNI example data, as in Diekmann & Przepiorka (2016), p.1318
+#----------------------------------------------------------------------------------------------------#
+createVodTestData2 <- function() {
+  round <- c(0,1,2,3,4,5,6,7,8,9,10)
+  player1 <- c(NA,"c","d","c","d","c","d","d","d","c","d")
+  player2 <- c(NA,"c","c","d","c","d","c","c","c","d","d")
+  player3 <- c(NA,"d","d","d","d","d","d","d","d","d","c")
+  util1 <- c(0,60,100,60,100,60,100,100,100,60,100)
+  util2 <- c(0,60,60,100,60,100,60,60,60,100,100)
+  util3 <- c(0,100,100,100,100,100,100,100,100,100,60)
+  return(data.frame(round,player1,player2,player3,util1,util2,util3))
+}
+
+#----------------------------------------------------------------------------------------------------#
+# function: testAnalysis
+#     Starting point for test analysis.
+#----------------------------------------------------------------------------------------------------#
+testAnalysis <- function() {
+  vodTestData <- createVodTestData1()
+  computeLNIs(vodTestData)
+  
+  vodTestData <- createVodTestData2()
+  computeLNIs(vodTestData)
+}
+
+
+
+
 
