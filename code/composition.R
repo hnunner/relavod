2087aa713f4eaf72585b8682caa39c28c36a0392
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2017-12-19 19:02:34 +0100

    parameter preparations for simulations

--- code/composition.R
+++ code/composition.R
@@ -45,11 +45,11 @@ simulateAndAnalyze <- function(modelType = MODEL_TYPES[3],
 fitParameters <- function() {
   
   # number of simulations and rounds per simulation
-  vodCount <- 4
-  roundsPerVod <- 50
+  vodCount <- 10
+  roundsPerVod <- 150
   
   # Random, ClassicQ, SequenceX
-  modelTypes <- MODEL_TYPES
+  modelTypes <- c(MODEL_TYPES[1], MODEL_TYPES[2], MODEL_TYPES[4])
   
   ##### Random #####
   # coop ratio
@@ -57,20 +57,20 @@ fitParameters <- function() {
   
   ##### ClassicQ, SequenceX #####
   # exploration vs. exploitation: e-noise
-  balancingTypes <- c(BALANCING_TYPES[2])       # e-noise
-  epsilonDecays <- c(1)                         # no decay
-  epsilonStarts <- c(0.1)                       # 0.1 = 1 standard deviation
+  balancingTypes <- c(BALANCING_TYPES[2])             # e-noise
+  epsilonDecays <- c(1)                               # no decay
+  epsilonStarts <- c(0.1, 0.2)                        # 0.X = X standard deviation(s)
   
   # social preference: selfish, altruistic
   sBehavs <- SOCIAL_BEHAVIORS
   
   # initial propensity
-  propStarts <- c(43.33)
+  propStarts <- c(43.33, 67.5)
   
   # learning rate
-  alphas <- c(0.20, 0.25)
+  alphas <- c(0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70)
   # discount rate
-  gammas <- c(0.75, 0.80)
+  gammas <- c(0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00)
   
   # ClassicQ 
   classicXs <- c(2)               # actions per state
